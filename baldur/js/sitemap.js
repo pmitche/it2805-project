26c1ecb22f15a099a7c9beb9f96bb509b377c1dc
kyrrelm <kyrre.l.moe@gmail.com>
2014-11-29 20:50:40 +0100

    more sitemap

--- baldur/js/sitemap.js
+++ baldur/js/sitemap.js
@@ -3,5 +3,12 @@ window.onload=function(){
     if(isLoggedIn()){
         $( "#dynSiteMap").empty();
         $( "#dynSiteMap" ).append('<li><a href="../profile/profile.html">Profil</li>');
+
+        $( "#upcomming").empty();
+        $( "#upcomming" ).append('<ul>' +
+            '<li><a href="../events/tennis.html">Norgesmesterskap i Tennis</a></li>' +
+            '<li><a href="../events/squash.html">Squash shoot-out</a></li>' +
+            '<li><a href="../events/anatomi.html">Eksamensforelesning i anatomi</a></li>' +
+            '<li><a href="../events/ekskursjon.html">Ekskursjon til OL i Pyongyang 2016</a></li></ul>');
     }
 };
